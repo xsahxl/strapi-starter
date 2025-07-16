10cf28b3566a153644c75c9f636264d008b21fd8
xsahxl <xsahxl@126.com>
2025-07-16 17:31:53 +0800

    Refactor category and restaurant relations in schema files. Updated the 'restaurants' attribute in category schema to reference 'api::restaurant.restaurant' and added 'categories' attribute in restaurant schema to reference 'api::category.category'. Adjusted TypeScript definitions accordingly.

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -421,10 +421,6 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
-    categories: Schema.Attribute.Relation<
-      'manyToMany',
-      'api::category.category'
-    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
@@ -440,7 +436,7 @@ export interface ApiCategoryCategory extends Struct.CollectionTypeSchema {
     publishedAt: Schema.Attribute.DateTime;
     restaurants: Schema.Attribute.Relation<
       'manyToMany',
-      'api::category.category'
+      'api::restaurant.restaurant'
     >;
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
@@ -459,6 +455,10 @@ export interface ApiRestaurantRestaurant extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    categories: Schema.Attribute.Relation<
+      'manyToMany',
+      'api::category.category'
+    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
